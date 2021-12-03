a12198e236336908192d911b89132a89595080c7
Daniel Appel <fake@name.net>
2021-12-03 16:11:29 +0000

    adding a compiler/packager for the alsa-lib installable library/progs

--- alsa/alsa-lib/compile.and.package.alsa-lib.sh
+++ alsa/alsa-lib/compile.and.package.alsa-lib.sh
@@ -1,4 +1,45 @@
 #!/bin/bash
 
-tar xvjf *.bz2
-cd alsa-lib*
+PROG_NAME="alsa-lib"
+PROG_VERSION="1.2.5.1"
+ARCHITECTURE="arm64"
+PKG_DESTINATION_PATH="$HOME/debpkgs/$PROG_NAME\_$PROG_VERSION\_$ARCHITECTURE"
+PROG_EXTERNAL_LOCATION="https://www.alsa-project.org/files/pub/lib/$PROG_NAME-$PROG_VERSION.tar.bz2"
+PROG_DESCRIPTION="ALSA (Advanced Linux Sound Architecture) is the next generation Linux Sound API."
+POST_INSTALL="yes"
+POST_INSTALL_INSTRUCTIONS="ldconfig"
+wget $PROG_EXTERNAL_LOCATION
+tar xvjf $PROG_NAME-$PROG_VERSION.tar.bz2
+cd $PROG_NAME-$PROG_VERSION
+mkdir -p $PKG_DESTINATION_PATH/DEBIAN
+git apply ../patches/*.patch
+./configure \
+	--without-debug \
+	--disable-depency-tracking \
+	--with-plugindir=/usr/lib/alsa \
+	--disable-python \
+	--prefix=$PKG_DESTINATION_PATH
+make -j 6
+make install
+cd ../
+cp -PR config/modprobe.d $PKG_DESTINATION_PATH/usr/config/
+mv $PKG_DESTINATION_PATH/share $PKG_DESTINATION_PATH/usr/
+mv $PKG_DESTINATION_PATH/include $PKG_DESTINATION_PATH/usr/
+
+
+# Print metadata into the control file
+printf "Package: $PROG_NAME\nVersion: $PROG_VERSION\nArchitecture: $ARCHITECTURE\nEssential: no\nPriority: optional\nDepends: none\nMaintainer: Daniel Appel\nDescription: $PROG_DESCRIPTION" > $PKG_DESTINATION_PATH/DEBIAN/control
+
+
+function post_install_creator {
+	if [[ $POST_INSTALL = 'yes' ]]; then
+		printf '#!/bin/bash\n' > $PKG_DESTINATION_PATH/DEBIAN/postinst
+		printf "$POST_INSTALL_INSTRUCTIONS" >> $PKG_DESTINATION_PATH/DEBIAN/postinst
+		chmod 755 $PKG_DESTINATION_PATH/DEBIAN/postinst 
+}
+
+
+post_install_creator
+
+# Create the deb package
+dpkg-deb --build $PKG_DESTINATION_PATH
