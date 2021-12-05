6d8b41db82926aa50de2d8580fde97b31411626f
Daniel Appel <fake@name.net>
2021-12-05 16:53:32 +0000

    adding ir-bpf-decoders

---
+++ ir-bpf-decoders/compile.and.package.ir-bpf-decoders.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+PROG_NAME="ir-bpf-decoders"
+PROG_VERSION="1.22.0"
+ARCHITECTURE="arm64"
+PKG_DESTINATION_PATH="$HOME/debpkgs/${PROG_NAME}_${PROG_VERSION}_${ARCHITECTURE}"
+PROG_EXTERNAL_LOCATION="https://github.com/LibreELEC/$PROG_NAME/archive/v4l-utils-$PROG_VERSION.tar.gz"
+PROG_DEPENDS="libc6"
+PROG_DESCRIPTION="ir-bpf-decoders: precompiled binaries of IR BPF decoders from v4l-utils utils/keytable/bpf_protocols"
+PRE_INSTALL="no"
+PRE_INSTALL_INSTRUCTIONS=""
+POST_INSTALL="yes"
+POST_INSTALL_INSTRUCTIONS="sudo ldconfig"
+wget $PROG_EXTERNAL_LOCATION
+tar xvj v4l-utils-$PROG_VERSION.tar.gz
+if [ -d $PKG_DESTINATION_PATH ]; then
+	rm -rf $PKG_DESTINATION_PATH
+fi
+mkdir -p $PKG_DESTINATION_PATH/DEBIAN
+mkdir -p $PKG_DESTINATION_PATH/usr/lib/udev/rc_keymaps/protocols
+cp -PR $PROG_NAME-4l-utils-$PROG_VERSION/*.o $PKG_DESTINATION_PATH/usr/lib/udev/rc_keymaps/protocols
+
+
+# Print metadata into the control file
+printf "Package: $PROG_NAME\nVersion: $PROG_VERSION\nArchitecture: $ARCHITECTURE\nEssential: no\nPriority: optional\nDepends: $PROG_DEPENDS\nMaintainer: Daniel Appel\nDescription: $PROG_DESCRIPTION\n" > $PKG_DESTINATION_PATH/DEBIAN/control
+
+function pre_install_creator {
+	if [[ $PRE_INSTALL = 'yes' ]]; then
+		printf '#!/bin/bash\n' > $PKG_DESTINATION_PATH/DEBIAN/preinst
+		printf "$PRE_INSTALL_INSTRUCTIONS" >> $PKG_DESTINATION_PATH/DEBIAN/preinstall
+		chmod 755 $PKG_DESTINATION_PATH/DEBIAN/preinstall
+	else
+		printf "\nno pre installation script required\n"
+	fi
+
+}
+
+function post_install_creator {
+	if [[ $POST_INSTALL = 'yes' ]]; then
+		printf '#!/bin/bash\n' > $PKG_DESTINATION_PATH/DEBIAN/postinst
+		printf "$POST_INSTALL_INSTRUCTIONS" >> $PKG_DESTINATION_PATH/DEBIAN/postinst
+		chmod 755 $PKG_DESTINATION_PATH/DEBIAN/postinst 
+	else
+		printf "\nno post installation script required\n"
+	fi
+}
+
+pre_install_creator
+post_install_creator
+
+# Create the deb package
+dpkg-deb --build $PKG_DESTINATION_PATH
