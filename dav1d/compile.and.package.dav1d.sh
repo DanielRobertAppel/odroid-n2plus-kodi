9b68fadc21faa5c2eed5ed7021d6ce7148ee6b35
Daniel Appel <fake@name.net>
2021-12-12 20:36:39 +0000

    adding dav1d to the list of packages

---
+++ dav1d/compile.and.package.dav1d.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+PROG_NAME="dav1d"
+PROG_VERSION="0.9.2"
+ARCHITECTURE="arm64"
+PKG_DESTINATION_PATH="$HOME/debpkgs/${PROG_NAME}_${PROG_VERSION}_${ARCHITECTURE}"
+PROG_EXTERNAL_LOCATION="https://code.videolan.org/videolan/dav1d/-/archive/$PROG_VERSION/dav1d-${PROG_VERSION}.tar.bz2"
+PROG_DEPENDS="libc6"
+PROG_DESCRIPTION="dav1d is an AV1 decoder :)"
+PRE_INSTALL="no"
+PRE_INSTALL_INSTRUCTIONS=""
+POST_INSTALL="yes"
+POST_INSTALL_INSTRUCTIONS="sudo ldconfig"
+wget $PROG_EXTERNAL_LOCATION
+tar xvjf $PROG_NAME-$PROG_VERSION.tar.bz2
+cd $PROG_NAME-$PROG_VERSION
+mkdir -p $PKG_DESTINATION_PATH/DEBIAN
+mkdir -p $PKG_DESTINATION_PATH/usr/config
+meson builddir/ \
+	-Denable_tools=false \
+	-Denable_tests=false \
+	--prefix=$PKG_DESTINATION_PATH
+ninja install
+cd ../
+
+# Print metadata into the control file
+printf "Package: $PROG_NAME\nVersion: $PROG_VERSION\nArchitecture: $ARCHITECTURE\nEssential: no\nPriority: optional\nDepends: $PROG_DEPENDS\nMaintainer: Daniel Appel\nDescription: $PROG_DESCRIPTION\n" > $PKG_DESTINATION_PATH/DEBIAN/control
+
+function pre_install_creator {
+	if [[ $PRE_INSTALL = 'yes' ]]; then
+		printf '#!/bin/bash\n' > $PKG_DESTINATION_PATH/DEBIAN/preinst
+		printf "$PRE_INSTALL_INSTRUCTIONS" >> $PKG_DESTINATION_PATH/DEBIAN/preinstall
+		chmod 755 $PKG_DESTINATION_PATH/DEBIAN/preinstall
+	else
+		printf "\nno pre installation script required\n"
+	fi
+
+}
+
+function post_install_creator {
+	if [[ $POST_INSTALL = 'yes' ]]; then
+		printf '#!/bin/bash\n' > $PKG_DESTINATION_PATH/DEBIAN/postinst
+		printf "$POST_INSTALL_INSTRUCTIONS" >> $PKG_DESTINATION_PATH/DEBIAN/postinst
+		chmod 755 $PKG_DESTINATION_PATH/DEBIAN/postinst 
+	else
+		printf "\nno post installation script required\n"
+	fi
+}
+
+pre_install_creator
+post_install_creator
+
+# Create the deb package
+dpkg-deb --build $PKG_DESTINATION_PATH
