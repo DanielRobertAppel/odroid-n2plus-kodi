5f2af4775ca077b0c864bbf4a4055f6efc832470
Daniel Appel <fake@name.net>
2022-01-01 13:31:46 +0000

    adding wireguard-tools

---
+++ wireguard-tools/compile.and.package.wireguard-tools.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+PROG_NAME="wireguard-tools"
+PROG_VERSION="1.0.20210914"
+ARCHITECTURE="arm64"
+PKG_DESTINATION_PATH="$HOME/debpkgs/${PROG_NAME}_${PROG_VERSION}_${ARCHITECTURE}"
+PROG_EXTERNAL_LOCATION="https://git.zx2c4.com/wireguard-tools/snapshot/wireguard-tools-v$PROG_VERSION.tar.xz"
+PROG_DEPENDS="libc6"
+PROG_DESCRIPTION="WireGuard VPN userspace tools"
+PRE_INSTALL="no"
+PRE_INSTALL_INSTRUCTIONS=""
+POST_INSTALL="special"
+POST_INSTALL_INSTRUCTIONS=""
+wget -O $PROG_NAME.tar.xz $PROG_EXTERNAL_LOCATION
+tar xvf $PROG_NAME-v$PROG_VERSION.tar.xz
+cd $PROG_NAME-v$PROG_VERSION/src
+mkdir -p $PKG_DESTINATION_PATH/DEBIAN
+mkdir -p $PKG_DESTINATION_PATH/usr/
+make -j 6
+make install PREFIX=$PKG_DESTINATION_PATH/usr/
+cp wg-quick/linux.bash $PKG_DESTINATION_PATH/
+cd ../../
+
+
+# Print metadata into the control file
+printf "Package: $PROG_NAME\nVersion: $PROG_VERSION\nArchitecture: $ARCHITECTURE\nEssential: no\nPriority: optional\nDepends: $PROG_DEPENDS\nMaintainer: Daniel Appel\nDescription: $PROG_DESCRIPTION\n" > $PKG_DESTINATION_PATH/DEBIAN/control
+
+function pre_install_creator {
+	if [[ $PRE_INSTALL = 'yes' ]]; then
+		printf '#!/bin/bash\n' > $PKG_DESTINATION_PATH/DEBIAN/preinst
+		printf "$PRE_INSTALL_INSTRUCTIONS" >> $PKG_DESTINATION_PATH/DEBIAN/preinstall
+		chmod 755 $PKG_DESTINATION_PATH/DEBIAN/preinstall
+	else
+		printf "\nno pre installation script required\n"
+	fi
+
+}
+
+function post_install_creator {
+	mv $PROG_NAME-v$PROG_VERSION/wg-quick/linux.bash $PKG_DESTINATION_PATH/DEBIAN/postinst
+	chmod 755 $PKG_DESTINATION_PATH/DEBIAN/postinst
+}
+
+pre_install_creator
+post_install_creator
+
+# Create the deb package
+dpkg-deb --build $PKG_DESTINATION_PATH
