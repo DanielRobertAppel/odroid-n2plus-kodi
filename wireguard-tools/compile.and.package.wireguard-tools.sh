e81f6a83f4d1c136a27d155289487156b901734f
Daniel Appel <fake@name.net>
2022-01-01 13:45:01 +0000

    fixing post-install for wireguard

--- wireguard-tools/compile.and.package.wireguard-tools.sh
+++ wireguard-tools/compile.and.package.wireguard-tools.sh
@@ -9,7 +9,7 @@ PROG_DEPENDS="libc6"
 PROG_DESCRIPTION="WireGuard VPN userspace tools"
 PRE_INSTALL="no"
 PRE_INSTALL_INSTRUCTIONS=""
-POST_INSTALL="special"
+POST_INSTALL="no"
 POST_INSTALL_INSTRUCTIONS=""
 wget $PROG_EXTERNAL_LOCATION
 tar xvf $PROG_NAME-v$PROG_VERSION.tar.xz
@@ -17,8 +17,7 @@ cd $PROG_NAME-v$PROG_VERSION/src
 mkdir -p $PKG_DESTINATION_PATH/DEBIAN
 mkdir -p $PKG_DESTINATION_PATH/usr/
 make -j 6
-make install PREFIX=$PKG_DESTINATION_PATH/usr/
-cp wg-quick/linux.bash $PKG_DESTINATION_PATH/
+make install PREFIX=$PKG_DESTINATION_PATH/usr
 cd ../../
 
 
@@ -37,8 +36,13 @@ function pre_install_creator {
 }
 
 function post_install_creator {
-	mv $PROG_NAME-v$PROG_VERSION/src/wg-quick/linux.bash $PKG_DESTINATION_PATH/DEBIAN/postinst
-	chmod 755 $PKG_DESTINATION_PATH/DEBIAN/postinst
+	if [[ $POST_INSTALL = 'yes' ]]; then
+		printf '#!/bin/bash\n' > $PKG_DESTINATION_PATH/DEBIAN/postinst
+		printf "$POST_INSTALL_INSTRUCTIONS" >> $PKG_DESTINATION_PATH/DEBIAN/postinst
+		chmod 755 $PKG_DESTINATION_PATH/DEBIAN/postinst 
+	else
+		printf "\nno post installation script required\n"
+	fi
 }
 
 pre_install_creator
